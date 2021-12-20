366bebb7cb6d6697177c50adc83a0e81d1ecff39
Jeffrey Tian <jefftian@umich.edu>
2021-12-20 01:22:24 -0800

    search function

--- app.py
+++ app.py
@@ -15,6 +15,46 @@ def get_db_connection():
 @app.route('/')
 def index():
     conn = get_db_connection()
-    results = conn.execute('SELECT * FROM sneakers').fetchall()
+    old_results = conn.execute('SELECT * FROM sneakers ORDER BY size ASC').fetchall()
     conn.close()
-    return render_template('index.html', results=results)
+
+    snkr_list = []
+    sku_dict = {}
+    for (snkrid, snkrname, sku, imgname, size, price) in old_results:
+        if sku not in sku_dict:
+            sku_dict[sku] = []
+            snkr_list.append((snkrname, sku, imgname))
+        sku_dict[sku].append((size, price))
+
+    results = {'snkr_list': snkr_list, 'sku_dict': sku_dict}
+    print(results)
+
+    return render_template('index.html', **results)
+
+@app.route('/search', methods = ['POST'])
+def search():
+    if request.method == 'POST':
+        form_data = request.form
+
+        search_val = form_data['search']
+
+        print("searched for: ", search_val)
+
+        conn = get_db_connection()
+        old_results = conn.execute('SELECT * FROM sneakers ORDER BY size ASC').fetchall()
+        conn.close()
+
+        snkr_list = []
+        sku_dict = {}
+        for (snkrid, snkrname, sku, imgname, size, price) in old_results:
+            if search_val.lower() not in snkrname.lower() and search_val.lower() not in sku.lower():
+                continue
+            if sku not in sku_dict:
+                sku_dict[sku] = []
+                snkr_list.append((snkrname, sku, imgname))
+            sku_dict[sku].append((size, price))
+
+        results = {'snkr_list': snkr_list, 'sku_dict': sku_dict}
+        print(results)
+
+        return render_template('index.html', **results)
